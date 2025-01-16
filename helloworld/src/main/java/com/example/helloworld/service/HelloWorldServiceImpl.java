fedd3a4e843d16dfb1699e3a810d6b9e2b2608f9
AhmadBadir <bdair2002@gmail.com>
2025-01-16 18:04:28 +0100

    creeate helloworld

---
+++ helloworld/src/main/java/com/example/helloworld/service/HelloWorldServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.helloworld.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldServiceImpl implements HellowWorldService  {
+
+
+    public HelloWorldServiceImpl() {
+
+    }
+    public String helloworld() {
+        return "Hello World from service!!";
+    }
+}
