109d231c18a94c369a6bc2fa9b7cefbd01f211ab
AhmadBadir <bdair2002@gmail.com>
2025-01-25 20:25:54 +0100

    open api configuration

--- helloworld/src/main/java/com/example/helloworld/controller/EmployeeController.java
+++ helloworld/src/main/java/com/example/helloworld/controller/EmployeeController.java
@@ -2,6 +2,10 @@ package com.example.helloworld.controller;
 
 import com.example.helloworld.model.Employee;
 import com.example.helloworld.service.EmployeeServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +27,10 @@ public class EmployeeController {
   }
 
   @GetMapping("/employees")
+  @Operation(summary = "Get all employees", description = "Returns a list of all employees", tags = {"Employee"})
+  @ApiResponse(responseCode = "200", description = "List of employees",
+          content = {@Content(mediaType = "application/json",
+                  schema = @Schema(implementation = Employee.class))})
   public ResponseEntity<List<Employee>> getAllEmployees() {
     return ResponseEntity.ok(employeeService.getAllEmployees());
   }
