1c5e522250d5581472ae939c3506f15a56187881
AhmadBadir <bdair2002@gmail.com>
2025-02-09 22:32:28 +0100

    Update GlobalExceptionHandler.java

--- helloworld/src/main/java/com/example/helloworld/exception/GlobalExceptionHandler.java
+++ helloworld/src/main/java/com/example/helloworld/exception/GlobalExceptionHandler.java
@@ -1,6 +1,21 @@
 package com.example.helloworld.exception;
 
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.crypto.BadPaddingException;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {}
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException exception) {
+    return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+}
